1f9590e511cd39c898f260f02cc38347be0e7e7b
Luciano Fernandes <lucianomafer@hotmail.com>
2024-01-26 15:23:02 -0300

    First Steps in Java with Spring Boot

---
+++ 5_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/lucianomafer/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.lucianomafer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
